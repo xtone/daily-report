a6e95676c820797e7b29488c9a9dcf4da05338b3
kibihara <k.kibihara@xtone.co.jp>
2016-10-27 20:20:24 +0900

    テストコードファイルを追加

---
+++ spec/factories/projects.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :project do
+    code 1
+    name "MyString"
+    name_reading "MyString"
+  end
+end
---
+++ spec/factories/user_roles.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :user_role do
+    role 1
+  end
+end
