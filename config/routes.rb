559f24c5808c7c86474dbcdd8d99eb38e7b692ea
kibihara <k.kibihara@xtone.co.jp>
2016-11-07 19:48:39 +0900

    routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'reports#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'users/sessions#destroy'
+    resources :users, only: [:index, :show]
+  end
+
+  resources :reports
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
