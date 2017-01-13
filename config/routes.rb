c58bbb52d65fefadcc667c463792aa2abf0fed11
kibihara <k.kibihara@xtone.co.jp>
2017-01-13 13:07:12 +0900

    routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'reports#index'
@@ -34,4 +36,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :csvs, only: [:index]
   end
+
+  mount Sidekiq::Web => '/sidekiq'
 end
