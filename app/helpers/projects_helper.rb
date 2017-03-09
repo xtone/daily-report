0b33c0484309806d1f41e92d5bd28675f48447f8
kibihara <k.kibihara@xtone.co.jp>
2017-03-09 15:12:52 +0900

    プロジェクト一覧にソート機能を追加

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,4 +6,16 @@ module ProjectsHelper
       tag(:span, class: 'glyphicon glyphicon-remove text-danger')
     end
   end
+
+  # @param [Symbol] model
+  # @param [Symbol] column
+  # @param [String] order_by
+  def index_header(model, column, order_by)
+    label = t("#{model}.#{column}")
+    if order_by == "#{column}_asc"
+      link_to "#{label} ▼", { order: "#{column}_desc" }, { title: "#{label}の降順にソート" }
+    else
+      link_to "#{label} ▲", { order: "#{column}_asc" }, { title: "#{label}の昇順にソート" }
+    end
+  end
 end
