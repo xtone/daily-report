17f7e378a88907e3e37f73d6f58cb2b386fff8f0
kibihara <k.kibihara@xtone.co.jp>
2017-03-09 15:12:18 +0900

    PJコードの新規候補の計算方法を修正

--- app/models/project.rb
+++ app/models/project.rb
@@ -38,8 +38,7 @@ class Project < ApplicationRecord
     # 頭2桁が年の下2桁、続く3桁が年内のプロジェクトの通し番号
     # @return [Integer]
     def next_expected_code(at = Time.zone.now)
-      this_year_projects = where(created_at: (at.beginning_of_year)..(at.end_of_year)).count
-      (at.year % 100) * 1000 + this_year_projects + 1
+      [(at.year % 100) * 1000, maximum(:code)].max + 1
     end
   end
 
