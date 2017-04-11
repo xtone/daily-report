cc25c395200db701c316c67884e9f069f6a935d2
kibihara <k.kibihara@xtone.co.jp>
2017-04-11 16:48:38 +0900

    Gmailアプリパスワードを環境変数にした（以前ベタ書きしていたパスワードは無効化済）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'gmail.com',
     user_name:            'k.kibihara@xtone.co.jp',
-    password:             'iejefculznbkwmzl',
+    password:             ENV['GOOGLE_APP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
