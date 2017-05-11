022ca104f8419c856c1f80210e5c061951c7202c
Yoichi Toyota <y.toyota@xtone.co.jp>
2017-05-11 17:20:06 +0900

    fix puma config for production.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/ec2-user/daily-report
-RAILS_ENV=production bin/bundle exec pumactl -F config/puma.rb start
+RAILS_ENV=production bin/bundle exec pumactl -P tmp/puma.pid -F config/puma_production.rb start
