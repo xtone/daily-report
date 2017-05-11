81e803d7a7ee4f101fca6940e14d8445a34f0e44
Yoichi Toyota <y.toyota@xtone.co.jp>
2017-05-11 17:03:06 +0900

    XAdd appspec.yml and setting for production.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+source /etc/environment
+source /home/ec2-user/.bash_profile
+
+export PATH RAILS_ENV RAILS_HOME DB_HOST DB_USERNAME DB_PASSWORD SECRET_KEY_BASE
+
 cd /home/ec2-user/daily-report
-RAILS_ENV=production bin/bundle exec pumactl -P tmp/puma.pid -F config/puma_production.rb start
+RAILS_ENV=production bin/bundle exec pumactl -F config/puma_production.rb start
