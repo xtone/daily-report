6574a28c0659641c4edfb1a9eb24a3acf65afb0c
kibihara <k.kibihara@xtone.co.jp>
2017-05-18 17:50:41 +0900

    fix test code.

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe AdminController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'when not login' do
+      it "redirected" do
+        get :index
+        expect(response).to redirect_to('/users/sign_in')
+      end
     end
   end
-
 end
+
