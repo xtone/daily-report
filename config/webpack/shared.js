c3273f6a390914566aebafd6c9c0a90388014979
kibihara <k.kibihara@xtone.co.jp>
2017-10-20 12:55:27 +0900

    Modify config

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -15,8 +15,6 @@ const extensionGlob = `**/*{${settings.extensions.join(',')}}*`
 const entryPath = join(settings.source_path, settings.source_entry_path)
 const packPaths = sync(join(entryPath, extensionGlob))
 
-const appConfig = require('./app-config.js')
-
 module.exports = {
   entry: packPaths.reduce(
     (map, entry) => {
@@ -37,14 +35,14 @@ module.exports = {
     rules: sync(join(loadersDir, '*.js')).map(loader => require(loader))
   },
 
-  plugins: appConfig.plugins.concat([
+  plugins: [
     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
     new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
     new ManifestPlugin({
       publicPath: output.publicPath,
       writeToFileEmit: true
     })
-  ]),
+  ],
 
   resolve: {
     extensions: settings.extensions,
