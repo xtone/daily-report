4e2e5438ffdb3a8ef04513ddb9209354057a292f
kibihara <k.kibihara@xtone.co.jp>
2017-11-17 15:15:23 +0900

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113072354) do
+ActiveRecord::Schema.define(version: 20171113121205) do
+
+  create_table "estimates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "project_id"
+    t.string "serial_no"
+    t.string "subject"
+    t.integer "amount", default: 0
+    t.float "director_manday", limit: 24, default: 0.0
+    t.float "engineer_manday", limit: 24, default: 0.0
+    t.float "designer_manday", limit: 24, default: 0.0
+    t.float "other_manday", limit: 24, default: 0.0
+    t.integer "cost", default: 0
+    t.date "estimated_on"
+    t.string "filename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_estimates_on_project_id"
+    t.index ["serial_no"], name: "index_estimates_on_serial_no", unique: true
+  end
 
   create_table "operations", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "report_id"
