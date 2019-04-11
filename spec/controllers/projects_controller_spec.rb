7f7ecb6bd0ff931c6861f5c7f11516b4ec1ac7fd
 田鶴浦咲 <tadzuura@xtonenoiMac.local>
2019-04-11 22:05:17 +0900

    factorygirlでuser_projectを生成するように修正

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,28 +1,21 @@
 RSpec.describe ProjectsController, type: :controller do
-  let!(:user) {create(:user, :administrator)}
-  let!(:project) {create(:project)}
+  let(:user) {create(:user, :administrator)}
+  let(:project) {create(:project, :with_user_project)}
 
   describe "delete #destroy" do
     before do
       sign_in user
+      delete :destroy, params: {id: project.id}
     end
 
     context 'project registerd to user_project table' do
-      before do
-        create(:user_project, user_id: user.id, project_id: project.id)
-        delete :destroy, params: {id: project.id}
-      end
-
       subject {Project.find_by(id: project.id)}
       it {is_expected.to be_present}
 
     end
 
     context 'project Not registerd to user_project table' do
-      before do
-        delete :destroy, params: {id: project.id}
-      end
-
+      let(:project) {create(:project)}
       subject {Project.find_by(id: project.id)}
       it {is_expected.not_to be_present}
     end
