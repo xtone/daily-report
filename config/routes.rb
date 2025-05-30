171f1649bc3d021d7d0f68cbe7e667d5ed9210d8
toyota <y.toyota@xtone.co.jp>
2025-05-30 13:28:38 +0900

    アプリケーションのタイムゾーンを東京に設定し、ルーティングを大幅に追加。新しいDockerfileを作成し、必要な依存関係をインストールする設定を追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,53 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'reports#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'users/sessions#destroy'
+    resources :users do
+      member do
+        patch 'revive'
+      end
+    end
+  end
+
+  resources :reports do
+    collection do
+      resource :summary, only: %i(show), module: :reports
+      resource :unsubmitted, only: %i(show), module: :reports
+    end
+  end
+  resources :projects do
+    resources :members, only: %i(index update destroy), module: :projects
+  end
+  resources :estimates, only: %i(index create) do
+    collection do
+      post :confirm
+    end
+  end
+  resources :bills, only: %i(index create) do
+    collection do
+      post :confirm
+    end
+  end
+
+  namespace :settings do
+    resources :projects, only: %i(index update destroy)
+    resource :password, only: %i(show update)
+  end
+
+  scope :admin do
+    root to: 'admin#index', as: :admin_root
+  end
+
+  namespace :admin do
+    resources :csvs, only: %i(index)
+  end
+
+  namespace :projects do
+    get 'members/edit'
+  end
 end
