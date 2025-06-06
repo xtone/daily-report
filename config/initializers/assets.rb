6e4a3730269df79fdd39bb20c5388e285eb13333
toyota <y.toyota@xtone.co.jp>
2025-06-06 12:15:01 +0900

    CoffeeScriptプロセッサーを無効化するための設定を追加しました。Reactへの移行が完了したため、互換性を保つために一時的にcoffee-railsを残しています。

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,26 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# CoffeeScriptプロセッサーを無効化（React移行完了のため）
+# 注意: 現在のSprocketsバージョンでは無効化メソッドが利用できないため、
+# coffee-railsを一時的に残してアセットパイプラインとの互換性を保つ
+# Rails.application.config.assets.configure do |env|
+#   # CoffeeScriptプロセッサーが存在する場合のみ無効化
+#   if defined?(Sprockets::CoffeeScriptProcessor)
+#     begin
+#       env.unregister_processor('application/javascript', Sprockets::CoffeeScriptProcessor)
+#     rescue NoMethodError
+#       # unregister_processorメソッドが存在しない場合は何もしない
+#     end
+#   end
+#   
+#   # CoffeeScriptトランスフォーマーを無効化（安全な方法）
+#   if env.respond_to?(:transformers) && env.transformers.respond_to?(:[])
+#     begin
+#       env.transformers.delete('text/coffeescript') if env.transformers['text/coffeescript']
+#     rescue
+#       # エラーが発生した場合は何もしない
+#     end
+#   end
+# end
